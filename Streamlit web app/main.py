cd3485d435aefdbf05af3a23432a6f7a6d0d394b
robbkeens <robbkeens@gmail.com>
2025-03-28 18:59:36 +0300

    Fixed module import issue in main.py

--- Streamlit web app/main.py
+++ Streamlit web app/main.py
@@ -1,18 +1,3 @@
-import subprocess
-import sys
-
-# Function to install missing packages
-def install_package(package):
-    subprocess.run([sys.executable, "-m", "pip", "install", package], check=True)
-
-# Try to import, install if missing
-try:
-    from streamlit_image_coordinates import streamlit_image_coordinates
-except ModuleNotFoundError:
-    install_package("streamlit-image-coordinates")
-    from streamlit_image_coordinates import streamlit_image_coordinates
-
-
 import tempfile
 import numpy as np
 
